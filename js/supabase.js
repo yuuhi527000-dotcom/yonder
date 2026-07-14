01a07e92eae8b320b28da8ebc243895116d8b0e2
yuuhi527000-dotcom <yuuhi527000@gmail.com>
2026-07-14 20:09:52 +0900

    Delete js directory

--- js/supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = 'https://muazseqaumtzzpsvogue.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11YXpzZXFhdW10enpwc3ZvZ3VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4ODAyNzAsImV4cCI6MjA5NTQ1NjI3MH0.5Gth7JImYUJwQZFvbAjgv6xu6FiYtUiyboUkMnTaoPE';
-const { createClient } = supabase;
-const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
